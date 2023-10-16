d70f7817410f68188cddc20cdf978c845e34f1af
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-10-16 13:24:52 +0600

    route and data handler

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,3 +1,11 @@
 export const GET =async(req: Request) =>{
     console.log("GET from id");
 };
+
+export const PUT =async(req: Request) =>{
+    console.log("PUT from id");
+};
+
+export const DELETE =async(req: Request) =>{
+    console.log("DELETE from id");
+};
