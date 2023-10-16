b3d8f9e66033e0e67109c96c0c205e57d2f75bb7
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-10-16 12:49:10 +0600

    created route files

---
+++ app/api/blogs/route.ts
@@ -0,0 +1,6 @@
+export const GET = async( req: Request, res: Response) =>{
+    console.log("Get Request");
+};
+export const POST = async( req: Request, res: Response) =>{
+    console.log("Post Request");
+};
