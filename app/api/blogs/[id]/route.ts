fe7640283569a3c943aa590d4aabc2712f0590ac
Arnisha-Akter <aakter191194@bscse.uiu.ac.bd>
2023-10-16 14:18:55 +0600

    put, get and post route

--- app/api/blogs/[id]/route.ts
+++ app/api/blogs/[id]/route.ts
@@ -1,5 +1,19 @@
-export const GET =async(req: Request) =>{
-    console.log("GET from id");
+import { addPost, getPosts, getById  } from "@/lib/data";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request) => {
+    try {
+        const id = req.url.split("blogs/")[1];
+        console.log(id);
+        const post = getById(id);
+        if (!post) {
+            return NextResponse.json({ message: "ERROR" }, { status: 404 });
+        }
+
+        return NextResponse.json({ message: "OK", post }, { status: 200 });
+    } catch (err) {
+        return NextResponse.json({ message: "ERROR", err }, { status: 500 });
+    }
 };
 
 export const PUT =async(req: Request) =>{
